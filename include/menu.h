58b2b8546b02fddf19bceb3a03772d5c2788e000
yue <yuenothingelse@gmail.com>
2026-06-07 13:08:19 +0700

    inisialisasi folder file header dan file header menu.h

---
+++ include/menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+void addSongMenu();
+
+void showCatalogMenu();
+
+void searchSongMenu();
+
+void updateSongMenu();
+
+void deleteSongMenu();
+
+void addPlaylistMenu();
+
+void showStatisticsMenu();
+
+#endif
